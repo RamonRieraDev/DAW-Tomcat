396ffaf7d7fd7443d04042b07281e4a050e605ef
RamonRieraDev <ramonrieradev@gmail.com>
2025-02-04 19:44:14 +0100

    Files_corrections
    He cambiado la estructura de los archivos y carpetas.

---
+++ MiSitioWeb/WEB-INF/src/HolaMundoServlet.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HolaMundoServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>¡Hola Mundo!</h1>");
+        out.println("<p>Bienvenido a MiSitioWeb</p>");
+        out.println("</body></html>");
+    }
+}
